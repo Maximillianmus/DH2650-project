95ebba6b9fcfeedccc8a047ff1f08b32caa4f202
Zezhe Huang <kiki_hzz@163.com>
2021-04-13 03:56:57 +0200

    add pause menu to basicpuzzle

--- DH2650/Assets/Scripts/PauseMenu.cs
+++ DH2650/Assets/Scripts/PauseMenu.cs
@@ -7,10 +7,11 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public bool lockedMouse = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,6 +34,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        if (lockedMouse)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     public void Pause()
@@ -40,12 +46,25 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        if (lockedMouse)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 
     public void LoadMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
+        GameIsPaused = false;
+    }
+
+    public void LoadLevelHub()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("LevelHub");
+        GameIsPaused = false;
     }
 
     public void QuitGame()
