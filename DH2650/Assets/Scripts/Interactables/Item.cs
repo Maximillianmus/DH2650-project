6315fa1f2dcedf952ae6c03e518fcd6413c1aba0
Erik <46922931+Teclised@users.noreply.github.com>
2021-04-13 20:18:25 +0200

    improvements to container, you can now hook items on them

---
+++ DH2650/Assets/Scripts/Interactables/Item.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Interactable
+{
+    // Used to make sure the object doesn't change anything about the player movement.
+    [SerializeField] int PlayerLayer = 7;
+    public bool inContainer = false;
+    public Container container;
+
+    private Collider coll;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        coll = this.GetComponent<Collider>();
+        rb = this.GetComponent<Collider>().gameObject.GetComponent<Rigidbody>();
+    }
+
+
+    // Define what happens when player interacts with item
+    public override void Interact(OffHand offHand)
+    {
+        // If it is in a container, let that handle how the interaction works
+        if(inContainer)
+        {
+            container.TakeItem(gameObject, offHand);
+        }
+        else
+        {
+            PickUp(offHand);
+        }
+    }
+
+    private void PickUp(OffHand offHand)
+    {
+        // If player has no item in offHand then take the item.
+        if(!offHand.slotFull)
+        {
+            offHand.slotFull = true;
+            offHand.heldItem = this.gameObject;
+            
+            // Move the item to the offhand
+            transform.SetParent(offHand.transform);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.Euler(Vector3.zero);
+            
+
+            // Remove collisions and physics and stuff
+            rb.isKinematic = true;
+            gameObject.layer = PlayerLayer;
+            coll.isTrigger = true;
+        }
+    }
+    public void ReleaseFromContainer()
+    {
+        if(inContainer)
+        {
+            transform.SetParent(null);
+            gameObject.layer = 6;
+
+            // Return collisions and physics
+            coll.isTrigger = false;
+            rb.isKinematic = false;
+
+            // update things in container
+            int pos = container.getIndexOfItem(gameObject);
+
+            container.containedItems[pos] = null;
+            container.usedContainers[pos] = false;
+            container.currentNumberOfItems--;
+
+            inContainer = false;
+            container = null;
+        }
+  
+    }
+}
--- DH2650/Assets/Scripts/Player/GrapplingGun.cs
+++ DH2650/Assets/Scripts/Player/GrapplingGun.cs
@@ -174,6 +174,9 @@ public class GrapplingGun : MonoBehaviour {
 
     void StartItemPull(RaycastHit hit)
     {
+        itemTransform = hit.transform;
+        itemTransform.gameObject.GetComponent<Item>().ReleaseFromContainer();
+
         isGrabbing = true;
         itemHitPoint = hit.point;
         harpoonItemOffset = Vector3.zero;
@@ -181,7 +184,7 @@ public class GrapplingGun : MonoBehaviour {
         itemRb = hit.transform.GetComponent<Rigidbody>();
         itemRb.useGravity = false;
         itemCollider = hit.transform.GetComponent<Collider>();
-        itemTransform = hit.transform;
+
         itemCollider.enabled = false;
        
 
