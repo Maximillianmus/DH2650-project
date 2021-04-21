ff51351538b29067e6a5e444b39b23e9d40f6fdd
Zezhe Huang <kiki_hzz@163.com>
2021-04-21 04:38:52 +0200

    cannon scripts

---
+++ DH2650/Assets/Scripts/CustomTowerTrigger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomTowerTrigger : MonoBehaviour {
+
+	public CustomTower twr;    
+    public bool lockE;
+	public GameObject curTarget;
+    
+
+
+    void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player") && !lockE)
+		{   
+			twr.target = other.gameObject.transform;            
+            curTarget = other.gameObject;
+			lockE = true;
+		}
+       
+    }
+	void Update()
+	{
+        if (curTarget)
+        {
+            //if (curTarget.CompareTag("Dead")) // get it from EnemyHealth
+            //{
+            //    lockE = false;
+            //    twr.target = null;               
+            //}
+        }
+
+
+
+
+        if (!curTarget) 
+		{
+			lockE = false;            
+        }
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.CompareTag("Player") && other.gameObject == curTarget)
+		{
+			lockE = false;
+            twr.target = null;            
+        }
+	}
+	
+}
