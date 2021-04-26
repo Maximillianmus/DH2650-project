0478a0bc24dfbb460ecd89011e3422096ce5a457
sigge94 <sigge.94@live.se>
2021-04-26 17:22:34 +0200

    player can heal from campfire when ineracting with it

--- DH2650/Assets/Scripts/Enemy/WaypointVisibility.cs
+++ DH2650/Assets/Scripts/Enemy/WaypointVisibility.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class WaypointVisibility : MonoBehaviour
 {
+    [Header("Makes the waypoint invisible once game is started", order = 0)]
+    [Space(-10, order = 1)]
+    [Header("Bad Design variable here so this header can exist", order = 2)]
+    /**
+     * Initialized so the header can actually exist
+     */
+    public char badDesign;
+
     // Start is called before the first frame update
     void Start()
     {
--- DH2650/Assets/Scripts/Player/HealthBar.cs
+++ DH2650/Assets/Scripts/Player/HealthBar.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
 
+    [Header("Don't change the health here!", order = 0)]
+    [Space(-10, order = 1)]
+    [Header("Change player health in PlayerHealth script!", order = 2)]
+
     public Slider slider;
     public Text text;
 
