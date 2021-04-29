600e5563802d77a036e01742ae2ae72876cc3485
sigge94 <sigge.94@live.se>
2021-04-29 17:50:54 +0200

    can change vignette programmatically

--- DH2650/Assets/Scripts/Player/PPTest.cs
+++ DH2650/Assets/Scripts/Player/PPTest.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hookblur : MonoBehaviour
+public class PPTest : MonoBehaviour
 {
-    GameObject globalVolume;
-    
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(-Vector3.forward * Time.deltaTime * Mathf.Sin(speed) * 100);
     }
 }
