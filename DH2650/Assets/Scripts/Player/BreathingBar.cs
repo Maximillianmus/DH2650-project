b0bd1261d3494fe939df289938f2a8b66cd56420
orrhsu <orrhsu@icloud.com>
2021-05-11 17:46:41 +0200

    Changes of health and breath bar
    Applied the assets of UI in gradient color

--- DH2650/Assets/Scripts/Player/BreathingBar.cs
+++ DH2650/Assets/Scripts/Player/BreathingBar.cs
@@ -6,23 +6,28 @@ using UnityEngine.UI;
 public class BreathingBar : MonoBehaviour
 {
 
-    public Slider slider;
+    public Image slider;
+    private float maxBreathingTime;
 
     public void SetBreathing(float health)
     {
-        slider.value = health;
-
+        //slider.value = health;
+        float amount = health / maxBreathingTime;
+        slider.fillAmount = amount;
     }
 
     public void SetMaxBreathing(float health)
     {
-        slider.maxValue = health;
-        slider.value = health;
+        //slider.maxValue = health;
+        //slider.value = health;
+        slider.fillAmount = 1;
+        maxBreathingTime = health;
 
     }
 
     public float getBreathingTime()
     {
-        return slider.maxValue;
+        //return slider.maxValue;
+        return maxBreathingTime;
     }
 }
