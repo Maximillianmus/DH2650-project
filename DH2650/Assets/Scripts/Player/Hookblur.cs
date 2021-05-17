f33abd7634ba2a83e656d505fcefe5f14240bfdf
sigge94 <sigge.94@live.se>
2021-05-17 12:33:53 +0200

    conf resolve

--- DH2650/Assets/Scripts/Player/Hookblur.cs
+++ DH2650/Assets/Scripts/Player/Hookblur.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Hookblur : MonoBehaviour
 {
-
+    GameObject globalVolume;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
