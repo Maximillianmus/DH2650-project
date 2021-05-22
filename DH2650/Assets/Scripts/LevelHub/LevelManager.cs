b0d88133db2752f711f226f16d4f2ef8f492aaea
Zezhe Huang <kiki_hzz@163.com>
2021-05-23 00:37:31 +0200

    level hub v.2

---
+++ DH2650/Assets/Scripts/LevelHub/LevelManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    private LevelPoint[] levels;
+    public TMP_Text mText;
+    private float distanceThreshold = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = gameObject.GetComponentsInChildren<LevelPoint>();
+        int sumScore = 0;
+        foreach (LevelPoint levelPoint in levels)
+        {
+            LevelData data = SaveSystem.LoadPlayer(levelPoint.SceneName);
+            if (data != null)
+                sumScore += data.Score;
+        }
+        mText.SetText($"{sumScore}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public LevelPoint GetClosestLevel(Transform transform)
+    {
+        float minDistance = 999;
+        LevelPoint closest = null;
+        foreach (LevelPoint levelPoint in levels)
+        {
+            float distance = Vector3.Distance(levelPoint.transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closest = levelPoint;
+            }
+        }
+
+        if (minDistance < distanceThreshold)
+        {
+            return closest;
+        }
+        return null;
+    }
+}
