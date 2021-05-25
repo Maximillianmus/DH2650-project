b034027e0a03a8a564cd761600035507cf4fe0b0
Zezhe Huang <kiki_hzz@163.com>
2021-05-25 22:40:15 +0200

    levelhub - avoid collider during path

--- DH2650/Assets/Scripts/LevelHub/BoatController.cs
+++ DH2650/Assets/Scripts/LevelHub/BoatController.cs
@@ -6,12 +6,14 @@ using UnityEngine.AI;
 public class BoatController : MonoBehaviour
 {
     public Camera cam;
+    public LevelPopUp levelPopUp;
     public NavMeshAgent agent;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown((0)))
+        if (levelPopUp.IsActive()) return;
+        if (Input.GetMouseButtonDown((0)))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
