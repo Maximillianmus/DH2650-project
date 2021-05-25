080956e961645a59be61e1730a553b11b5c13380
Zezhe Huang <kiki_hzz@163.com>
2021-05-25 22:39:30 +0200

    pausemenu setting & music

--- DH2650/Assets/Scripts/LevelHub/LevelInfo.cs
+++ DH2650/Assets/Scripts/LevelHub/LevelInfo.cs
@@ -7,9 +7,9 @@ public class LevelInfo
     // int[2]: Min score to unlock a scene, and full score. 
     public static Dictionary<string, int[]> ScoresInfo = new Dictionary<string, int[]>
     {
-        {"Tutorial", new int[]{0, 4, } },
-        {"SkyLevel", new int[]{1, 4, } },
+        {"Tutorial", new int[]{0, 3, } },
+        {"SkyLevel", new int[]{1, 3, } },
         {"Cave", new int[]{1, 4, } },
-        {"WaterLevel", new int[]{1, 4, } },
+        {"WaterLevel", new int[]{1, 5, } },
     };
 }
