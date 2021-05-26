70853bf637af10b188120f161b9a342e24db5fd2
Zezhe Huang <kiki_hzz@163.com>
2021-05-26 21:56:02 +0200

    savepoint init false

--- DH2650/Assets/Scripts/LevelHub/LevelInfo.cs
+++ DH2650/Assets/Scripts/LevelHub/LevelInfo.cs
@@ -8,9 +8,9 @@ public class LevelInfo
     public static Dictionary<string, int[]> ScoresInfo = new Dictionary<string, int[]>
     {
         {"Tutorial", new int[]{0, 3, } },
-        {"SkyLevel", new int[]{2, 3, } },
-        {"Cave", new int[]{4, 6, } },
-        {"WaterLevel", new int[]{3, 5, } },
+        {"SkyLevel", new int[]{0, 3, } },
+        {"Cave", new int[]{0, 6, } },
+        {"WaterLevel", new int[]{0, 5, } },
     };
 
     public static int TotalScore()
